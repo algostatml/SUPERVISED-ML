d6ff01f96e0c5968a2a09e1fcb85e452f42a360b
kennedy <kennyjunior022006@yahoo.com>
2019-10-01 10:36:59 +0200

    updated gradient function
    updated gradient function

--- REGRESSION/SupportVectorMachine(SVM)/svm.py
+++ REGRESSION/SupportVectorMachine(SVM)/svm.py
@@ -44,7 +44,7 @@ class linearSVM(loss):
         '''
         return y*np.dot(X, beta)
         
-    def fit(self, X, y, alpha = None, iterations = None):
+    def fit(self, X, y, alpha:float = None, iterations:int = None):
         if not alpha:
             alpha = 1e-5
             self.alpha = alpha
@@ -58,11 +58,13 @@ class linearSVM(loss):
         self.beta = np.random.randn(X.shape[1])
         self.cost_rec = np.zeros(self.iterations)
         self.beta_rec = np.zeros((self.iterations, X.shape[1]))
-        for _ in range(self.iterations):
+        for ii in range(self.iterations):
             self.margin = self.margins(X, y, self.beta)
             #adjust parameters according to misclafication
-            indices = np.where(self.margin < 0)
-            self.beta = self.beta - self.alpha * self.C * y[indices].dot(X[indices])
+            indices = np.where(self.margin < 1)
+            self.beta = self.beta - self.alpha*(self.beta - self.C * y[indices].dot(X[indices]))
+            self.beta_rec[ii, :] = self.beta.T
+            self.cost_rec[ii] = self.cost(X, y, self.beta)
             print(f"cost is {self.cost(X, y, self.beta)}")
         return self
     
@@ -98,7 +100,7 @@ class StochasticlinearSVM(loss):
         '''
         return 0.5 * beta.dot(beta) + self.C * np.sum(loss.hinge(X, y, beta))
     
-    def margins(self, X, y, beta):
+    def margins(self, X, y, beta:float):
         '''
         :param: X: NxD
         :param: y: Nx1
@@ -106,7 +108,7 @@ class StochasticlinearSVM(loss):
         '''
         return y*np.dot(X, beta)
         
-    def fit(self, X, y, alpha = None, iterations = None):
+    def fit(self, X, y, alpha:float = None, iterations:int = None):
         if not alpha:
             alpha = 1e-5
             self.alpha = alpha
@@ -121,7 +123,7 @@ class StochasticlinearSVM(loss):
         self.cost_rec = np.zeros(self.iterations)
         self.beta_rec = np.zeros((self.iterations, X.shape[1]))
         ylen = len(y)
-        for _ in range(self.iterations):
+        for ii in range(self.iterations):
             for ij in range(ylen):
                 random_samples = np.random.randint(1, ylen)
                 X_samp = X[:random_samples]
@@ -129,7 +131,9 @@ class StochasticlinearSVM(loss):
                 self.margin = self.margins(X_samp, y_samp, self.beta)
                 #adjust parameters according to misclafication
                 indices = np.where(self.margin < 0)
-                self.beta = self.beta - self.alpha * self.C * y_samp[indices].dot(X_samp[indices])
+                self.beta = self.beta - self.alpha*(self.beta - self.C * y_samp[indices].dot(X_samp[indices]))
+                self.beta_rec[ii, :] = self.beta.T
+                self.cost_rec[ii] += self.cost(X, y, self.beta)
                 print(f"cost is {self.cost(X, y, self.beta)}")
         return self
     
@@ -146,9 +150,10 @@ lsvm = linearSVM().fit(X_train, Y_train)
 lsvm.predict(X_test)
 plt.scatter(X_test[:, 0], X_test[:, 1], c = lsvm.predict(X_test))
 np.mean(lsvm.predict(X_test) == Y_test)
-
+plt.plot(np.arange(lsvm.iterations), lsvm.cost_rec)
 
 slsvm = StochasticlinearSVM().fit(X_train, Y_train)
 slsvm.predict(X_test)
 plt.scatter(X_test[:, 0], X_test[:, 1], c = slsvm.predict(X_test))
 np.mean(slsvm.predict(X_test) == Y_test)
+plt.plot(np.arange(slsvm.iterations), slsvm.cost_rec)
