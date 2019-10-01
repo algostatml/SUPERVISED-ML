6cca4e736a7e526c1d82ae5eff98b66b8988de14
kennedy <kennyjunior022006@yahoo.com>
2019-10-01 02:18:14 +0200

    Added Stochastic Linear SVM
    Added Stochastic Linear SVM

--- REGRESSION/SupportVectorMachine(SVM)/svm.py
+++ REGRESSION/SupportVectorMachine(SVM)/svm.py
@@ -11,6 +11,7 @@ import numpy as np
 from Utils.utils import EvalR
 from Utils.Loss import loss
 
+
 class linearSVM(loss):
     def __init__(self, C = None):
         '''
@@ -72,7 +73,73 @@ class linearSVM(loss):
                 yhat[enum] = 1
         return yhat
     
-            
+
+class StochasticlinearSVM(loss):
+    def __init__(self, C = None):
+        '''
+        Linear SVM via Stochastic Gradient descent
+        :params: C: misclassification penalty. 
+                    Default value is 0.1.
+        '''
+        if not C:
+            C = 1.0
+            self.C = C
+        else:
+            self.C = C
+        return
+    
+    def cost(self, X, y, beta):
+        '''
+        Hinge loss function
+        ------------------
+        :params: X: feature space
+        :params: y: target
+        :params: beta: weights parameters.
+        '''
+        return 0.5 * beta.dot(beta) + self.C * np.sum(loss.hinge(X, y, beta))
+    
+    def margins(self, X, y, beta):
+        '''
+        :param: X: NxD
+        :param: y: Nx1
+        :param: beta: Dx1
+        '''
+        return y*np.dot(X, beta)
+        
+    def fit(self, X, y, alpha = None, iterations = None):
+        if not alpha:
+            alpha = 1e-5
+            self.alpha = alpha
+        else:
+            self.alpha = alpha
+        if not iterations:
+            iterations = 500
+            self.iterations = iterations
+        else:
+            self.iterations = iterations
+        self.beta = np.random.randn(X.shape[1])
+        self.cost_rec = np.zeros(self.iterations)
+        self.beta_rec = np.zeros((self.iterations, X.shape[1]))
+        ylen = len(y)
+        for _ in range(self.iterations):
+            for ij in range(ylen):
+                random_samples = np.random.randint(1, ylen)
+                X_samp = X[:random_samples]
+                y_samp = y[:random_samples]
+                self.margin = self.margins(X_samp, y_samp, self.beta)
+                #adjust parameters according to misclafication
+                indices = np.where(self.margin < 0)
+                self.beta = self.beta - self.alpha * self.C * y_samp[indices].dot(X_samp[indices])
+                print(f"cost is {self.cost(X, y, self.beta)}")
+        return self
+    
+    def predict(self, X):
+        yhat:int = np.zeros(X.shape[0])
+        for enum, ii in enumerate(np.sign(X.dot(self.beta))):
+            if ii >0:
+                yhat[enum] = 1
+        return yhat
+              
 #%%
 import matplotlib.pyplot as plt
 lsvm = linearSVM().fit(X_train, Y_train)
@@ -80,3 +147,8 @@ lsvm.predict(X_test)
 plt.scatter(X_test[:, 0], X_test[:, 1], c = lsvm.predict(X_test))
 np.mean(lsvm.predict(X_test) == Y_test)
 
+
+slsvm = StochasticlinearSVM().fit(X_train, Y_train)
+slsvm.predict(X_test)
+plt.scatter(X_test[:, 0], X_test[:, 1], c = slsvm.predict(X_test))
+np.mean(slsvm.predict(X_test) == Y_test)
