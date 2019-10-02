a3e8f727d630fb1aef9d31edcc40209e94833675
KennedyWolfie <kennyjunior022006@yahoo.com>
2019-10-02 22:34:33 +0200

    Update kernels.py

--- Utils/kernels.py
+++ Utils/kernels.py
@@ -14,8 +14,9 @@ class Kernels:
     '''Docstring
     Kernels are mostly used for solving
     non-lineaar problems. By projecting/transforming
-    our data into a subspace that promote classification
-    performance.
+    our data into a subspace, making it easy to
+    almost accurately classify our data as if it were
+    still in linear space.
     '''
     def __init__(self):
         return
