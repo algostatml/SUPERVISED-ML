121621eafa2de7f86ec19d7c1c1abf24dfbdb005
kennedy <kennyjunior022006@yahoo.com>
2019-10-03 00:03:41 +0200

    updated with polynomial kernel
    updated with polynomial kernel

--- Utils/kernels.py
+++ Utils/kernels.py
@@ -22,13 +22,27 @@ class Kernels:
     
     @staticmethod
     def linear(x1, x2):
+        '''
+        Linear kernel
+        ----------------------------
+        :param: x1: NxD transposed feature space
+        :param: x2: NxD feature space
+        :return type: kernel(Gram) matrix
+        '''
         return x1.T.dot(x2)
     
     @staticmethod
     def rbf(x1, x2, gamma = None):
+        '''
+        RBF: Radial basis function or guassian kernel
+        ----------------------------------------------
+        :param: x1: NxD transposed feature space
+        :param: x2: NxD feature space
+        :param: gamma: 1/2(sigma-square)
+        :return type: kernel(Gram) matrix
+        '''
         if not gamma:
             gamma = 1.0/x1.shape[1]
-            
         if x1.ndim == 1 and x2.ndim == 1:
             return np.exp(-gamma * np.linalg.norm(x1 - x2)**2)
         elif (x1.ndim > 1 and x2.ndim == 1) or (x1.ndim == 1 and x2.ndim > 1):
@@ -38,8 +52,30 @@ class Kernels:
         
     @staticmethod
     def sigmoid(x1, x2, gamma = None, C = None):
+        '''
+        logistic or sigmoid kernel
+        ----------------------------------------------
+        :param: x1: NxD transposed feature space
+        :param: x2: NxD feature space
+        :param: gamma: 1/2(sigma-square)
+        :return type: kernel(Gram) matrix
+        '''
         if not gamma:
             gamma = .05
         if not C:
             C = 1
-        return np.tanh(gamma * x1.T.dot(x2))
+        return np.tanh(gamma * x1.T.dot(x2) + C)
+    
+    @staticmethod
+    def polynomial(x1, x2, d = None):
+        '''
+        RBF: Radial basis function or guassian kernel
+        ----------------------------------------------
+        :param: x1: NxD transposed feature space
+        :param: x2: NxD feature space
+        :param: d: polynomial degree
+        :return type: kernel(Gram) matrix
+        '''
+        if not d:
+            d = 2
+        return (x1.T.dot(x2))**d
