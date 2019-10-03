5d112688ec485fd50440e2d024b612f92bc8c797
kennedy <kennyjunior022006@yahoo.com>
2019-10-03 16:56:51 +0200

    updated kernel SVM
    updated kernel SVM

--- REGRESSION/SupportVectorMachine(SVM)/kernelSVM.py
+++ REGRESSION/SupportVectorMachine(SVM)/kernelSVM.py
@@ -7,11 +7,11 @@ Created on Tue Oct  1 13:28:19 2019
 """
 from __future__ import absolute_import
 import numpy as np
-from Utils.utils import EvalR
+from Utils.utils import EvalC
 from Utils.Loss import loss
 from Utils.kernels import Kernels
 
-class kSVM(EvalR, loss, Kernels):
+class kSVM(EvalC, loss, Kernels):
     '''
     Kernelized SVM via Gradient ascent.
     -----------------------------------
@@ -19,6 +19,7 @@ class kSVM(EvalR, loss, Kernels):
     for kernel SVMs.
     '''
     def __init__(self, kernel = None, C = None):
+        super().__init__()
         if not kernel:
             kernel = 'rbf' #default
             self.kernel = kernel
@@ -37,40 +38,40 @@ class kSVM(EvalR, loss, Kernels):
         '''
         return np.outer(y, y)
        
-    def kernellize(self, X):
+    def kernelize(self, x1, x2):
         '''
         :params: X: NxD
         '''
         if self.kernel == 'linear':
-            return Kernels.linear(X, X)
+            return Kernels.linear(x1, x2)
         elif self.kernel == 'rbf':
-            return Kernels.rbf(X, X)
+            return Kernels.rbf(x1, x2)
         elif self.kernel == 'sigmoid':
-            return Kernels.sigmoid(X, X)
+            return Kernels.sigmoid(x1, x2)
         elif self.kernel == 'polynomial':
-            return Kernels.polynomial(X, X)
+            return Kernels.polynomial(x1, x2)
         elif self.kernel == 'cosine':
-            return Kernels.cosine(X, X)
+            return Kernels.cosine(x1, x2)
         elif self.kernel == 'correlation':
-            return Kernels.correlation(X, X)
+            return Kernels.correlation(x1, x2)
         
         
     def alpha_y_i_kernel(self, X, y):
         '''
-        :params: X:
-        :params: y:
+        :params: X: NxD feature space
+        :params: y: Dx1 dimension
         '''
         alpha = np.zeros(X.shape[0])
-        self.alph_s = np.outer(alpha, alpha) #alpha_i alpha_j
-        self.y_i_s = self.y_i(y) #y_i y_j
-        self.kernel = self.kernellize(X) #Using the RBF kernel to get Gram matrix
-        return (alpha, self.alph_s, self.y_i_s, self.kernel)
+        self.alph_s = np.outer(alpha, alpha) #alpha_i's alpha_j's
+        self.y_i_s = self.y_i(y) #y_i's y_j's
+        self.k = self.kernelize(X, X)
+        return (alpha, self.alph_s, self.y_i_s, self.k)
         
     def cost(self):
         '''
         return type: x E R
         '''
-        return np.dot(self.alpha, np.ones(self.X.shape[0])) - .5 * np.sum(self.alpha_i_s * self.kernel * self.y_i_s )
+        return np.dot(self.alpha, np.ones(self.X.shape[0])) - .5 * np.sum(self.alpha_i_s * self.knl * self.y_i_s )
     
     def fit(self, X, y, lr:float = None, iterations:int = None):
         '''
@@ -91,13 +92,13 @@ class kSVM(EvalR, loss, Kernels):
             self.iteration = iterations
         else:
             self.iteration = iterations
-        self.alpha, self.alpha_i_s, self.y_i_s,  self.kernel = self.alpha_y_i_kernel(X, y)
+        self.alpha, self.alpha_i_s, self.y_i_s,  self.knl = self.alpha_y_i_kernel(X, y)
         cost = np.zeros(iterations)
         for ii in  range(self.iteration):
             cost[ii] = self.cost()
             print(f"Cost of computation: {cost[ii]}")
             #perform gradient ascent for maximization.
-            self.alpha = self.alpha + self.lr * (np.ones(X.shape[0]) - np.dot(self.y_i_s * self.kernel, self.alpha))
+            self.alpha = self.alpha + self.lr * (np.ones(X.shape[0]) - np.dot(self.y_i_s * self.knl, self.alpha))
             #0 < alpha < C
             self.alpha[self.alpha < 0 ] = 0
             self.alpha[self.alpha > self.C] = self.C
@@ -105,12 +106,11 @@ class kSVM(EvalR, loss, Kernels):
         return self
     
     def predict(self, X):
-        yhat:int = np.sign(np.dot(self.alpha * self.Y, Kernels.rbf(self.X, X)))
+        yhat:int = np.sign(np.dot(self.alpha * self.Y, self.kernelize(self.X, X)))
         for enum, ii in enumerate(yhat):
             if ii <0:
                 yhat[enum] = 0
         return yhat
-            
             
             
 #%% Testing
@@ -120,9 +120,10 @@ from sklearn.model_selection import train_test_split
 X, y = make_blobs(n_samples=100, centers=2, n_features=2)
 X = np.c_[np.ones(X.shape[0]), X]
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.3)
-kernelsvm = kSVM(kernel='sigmoid').fit(X_train, Y_train)
+kernelsvm = kSVM(kernel='linear').fit(X_train, Y_train)
 kernelsvm.predict(X_test)
 np.mean(kernelsvm.predict(X_test) == Y_test)
+kernelsvm.summary(Y_test, kernelsvm.predict(X_test), kernelsvm.alpha)
 plt.scatter(X_test[:, 0], X_test[:, 1], c = kernelsvm.predict(X_test))          
             
             
