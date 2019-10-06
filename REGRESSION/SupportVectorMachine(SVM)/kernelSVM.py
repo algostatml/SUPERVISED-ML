7fd33c0ae293ad2d802f9f552d7b9c84be92bd95
kennedy <kennyjunior022006@yahoo.com>
2019-10-06 18:40:29 +0200

    Updated SVM. Compare with sklearn version
    Updated SVM. Compare with sklearn version

--- REGRESSION/SupportVectorMachine(SVM)/kernelSVM.py
+++ REGRESSION/SupportVectorMachine(SVM)/kernelSVM.py
@@ -62,7 +62,7 @@ class kDualSVM(EvalC, loss, Kernels):
         :params: X: NxD feature space
         :params: y: Dx1 dimension
         '''
-        alpha = np.zeros(X.shape[0])
+        alpha = np.random.randn(X.shape[0])
         self.alph_s = np.outer(alpha, alpha) #alpha_i's alpha_j's
         self.y_i_s = self.y_i(y) #y_i's y_j's
         self.k = self.kernelize(X, X)
@@ -84,16 +84,17 @@ class kDualSVM(EvalC, loss, Kernels):
         self.X = X
         self.Y = y
         if not lr:
-            lr = 1e-5
+            lr = 1e-2
             self.lr = lr
         else:
             self.lr = lr
         if not iterations:
-            iterations = 500
+            iterations = 20
             self.iteration = iterations
         else:
             self.iteration = iterations
         self.alpha, self.alpha_i_s, self.y_i_s,  self.knl = self.alpha_y_i_kernel(self.X, self.Y)
+        self.b = 0
         cost = np.zeros(iterations)
         for ii in  range(self.iteration):
             cost[ii] = self.cost()
@@ -103,17 +104,22 @@ class kDualSVM(EvalC, loss, Kernels):
             #0 < alpha < C
             self.alpha[self.alpha < 0] = 0
             self.alpha[self.alpha > self.C] = self.C
-            
+        indices = np.where((self.alpha > 0) & (self.alpha < self.C))
+        self.b = self.Y[indices] - np.dot(self.alpha * self.Y, self.kernelize(self.X, self.X[indices]))
+        self.b = np.mean(self.b)
+        self.support_vectors = indices
+        print(f'Total support vectors required for classification: {len(self.support_vectors)}')
         return self
     
     def predict(self, X):
-        yhat:int = np.sign(np.dot(self.alpha * self.Y, self.kernelize(self.X, X)))
+        yhat:int = np.sign(np.dot(self.alpha * self.Y, self.kernelize(self.X, X)) + self.b)
         for enum, ii in enumerate(yhat):
             if ii <=0:
                 yhat[enum] = 0
         return yhat
             
 #%%
+        
 class kprimalSVM(EvalC, loss, Kernels):
     '''
     Kernelized SVM via Gradient ascent.
@@ -164,7 +170,7 @@ class kprimalSVM(EvalC, loss, Kernels):
         :params: X: NxD feature space
         :params: y: Dx1 dimension
         '''
-        alpha = np.zeros(X.shape[0])
+        alpha = np.random.randn(X.shape[0])
         self.alph_s = np.outer(alpha, alpha) #alpha_i's alpha_j's
         self.y_i_s = self.y_i(y) #y_i's y_j's
         self.k = self.kernelize(X, X)
@@ -174,13 +180,13 @@ class kprimalSVM(EvalC, loss, Kernels):
         '''
         Hinge loss for the dual primal
         '''
-        return np.maximum(0, 1 - self.y_i_s * self.alpha * self.kernelize(self.X, self.X))
+        return np.maximum(0, 1 - self.Y * (self.alpha.dot(self.kernelize(self.X, self.X)) + self.b))
     
     def cost(self):
         '''
         return type: x E R
         '''
-        return np.sum(self.alpha_i_s * self.knl * self.y_i_s ) + self.C * np.sum(self.hingeloss())
+        return .5* self.alpha.dot(self.knl.dot(self.alpha)) + self.C * np.sum(self.hingeloss())
     
     def fit(self, X, y, lr:float = None, iterations:int = None):
         '''
@@ -191,54 +197,74 @@ class kprimalSVM(EvalC, loss, Kernels):
         '''
         self.X = X
         self.Y = y
+        self.b = 0
         if not lr:
-            lr = 1e-5
+            lr = 1e-2
             self.lr = lr
         else:
             self.lr = lr
         if not iterations:
-            iterations = 500
+            iterations = 20
             self.iteration = iterations
         else:
             self.iteration = iterations
         self.alpha, self.alpha_i_s, self.y_i_s,  self.knl = self.alpha_y_i_kernel(X, y)
         cost = np.zeros(iterations)
+        self.cost_rec = np.zeros(self.iteration)
         for ii in  range(self.iteration):
+            self.margin = self.Y * (self.alpha.dot(self.kernelize(self.X, self.X)) + self.b)
+            indices = np.where(self.margin < 1)
             cost[ii] = self.cost()
             print(f"Cost of computation: {cost[ii]}")
             #perform gradient ascent for maximization.
-            self.alpha = self.alpha - self.lr * (np.dot(self.y_i_s * self.knl, self.alpha) - np.dot(self.Y * self.knl, self.Y))
-            #0 < alpha < C
-            self.alpha[self.alpha < 0 ] = 0
-            self.alpha[self.alpha > self.C] = self.C
-            
+            self.alpha = self.alpha - self.lr * (self.knl.dot(self.alpha) - self.C * self.Y[indices].dot(self.knl[indices]))
+            self.b = self.b - self.lr * self.C * np.sum(self.Y[indices])
+        #the support vectors are datapoints exactly on the margin
+        self.support_vectors = np.where((self.Y * (self.alpha.dot(self.kernelize(self.X, self.X)) + self.b)) <= 1)[0]
+        print(f'Total support vectors required for classification: {len(self.support_vectors)}')
         return self
     
     def predict(self, X):
-        yhat:int = np.sign(np.dot(self.alpha * self.Y, self.kernelize(self.X, X)))
+        yhat:int = np.sign(np.dot(self.alpha * self.Y, self.kernelize(self.X, X)) + self.b)
         for enum, ii in enumerate(yhat):
             if ii <0:
                 yhat[enum] = 0
         return yhat
     
     
+    
+    
 #%% Testing
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_moons
 from sklearn.model_selection import train_test_split
+X, y = make_moons(1000)
 X, y = make_blobs(n_samples=1000, centers=2, n_features=2)
-X = np.c_[np.ones(X.shape[0]), X]
+#X = np.c_[np.ones(X.shape[0]), X]
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.3)
-kernelsvm = kDualSVM(kernel='linear').fit(X_train, Y_train)
+kernelsvm = kDualSVM(kernel='rbf').fit(X_train, Y_train)
 kernelsvm.predict(X_test)
 kernelsvm.summary(Y_test, kernelsvm.predict(X_test), kernelsvm.alpha)
-plt.scatter(X_test[:, 1], X_test[:, 2], c = kernelsvm.predict(X_test))          
+plt.scatter(X_test[:, 0], X_test[:, 1], c = kernelsvm.predict(X_test))          
 
 
 primalkernelsvm = kprimalSVM(kernel='linear').fit(X_train, Y_train)
 primalkernelsvm.predict(X_test)
 primalkernelsvm.summary(Y_test, primalkernelsvm.predict(X_test), primalkernelsvm.alpha)
-plt.scatter(X_test[:, 1], X_test[:, 2], c = primalkernelsvm.predict(X_test))          
-            
+plt.scatter(X_test[:, 0], X_test[:, 1], c = primalkernelsvm.predict(X_test))          
+plt.plot(np.arange(primalkernelsvm.iteration), primalkernelsvm.cost_rec)            
        
-            
+           
+#%% Testing SVC from sklearn
+
+from sklearn.svm import SVC
+svc = SVC(C = 1.0, kernel = 'rbf',gamma='auto')
+svc.fit(X_train, Y_train)
+plt.scatter(X_test[:, 0], X_test[:, 1], c = svc.predict(X_test))
+
+
+
+
+
+
+
